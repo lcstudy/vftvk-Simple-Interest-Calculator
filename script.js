686e85445a5388caf97743c7daa17a2aea17bde6
lcstudy <88136552+lcstudy@users.noreply.github.com>
2021-07-30 00:03:11 +0700

    complete task

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var result = document.getElementById("result");
+  result.innerHTML = "";
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    result.innerHTML =
+      "<br/>" +
+      "If you deposit <mark>" +
+      principal +
+      "</mark><br/>" +
+      "at an interest rate of <mark>" +
+      rate +
+      "%</mark>.<br/>" +
+      "You will receive an amount of <mark>" +
+      interest +
+      "</mark>,</br>" +
+      "in the year <mark>" +
+      year +
+      "</mark>";
+  }
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
+
+function validatePrincipal() {}
